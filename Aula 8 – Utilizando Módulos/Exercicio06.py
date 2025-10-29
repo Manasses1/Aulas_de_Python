13be9ca6a295361181d84221f920035b4f4d5526
Manassés Freitas <manasses.freitas@acertonline.com>
2025-10-29 15:48:00 -0300

    mensagem explicando o que foi alterado

--- Aula 8 – Utilizando Módulos/Exercicio06.py
+++ Aula 8 – Utilizando Módulos/Exercicio06.py
@@ -1,1 +1,7 @@
 #   Faça um programa em Python que abre e reproduza o audio de um arquivo MP3.
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('Exercicio06.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
